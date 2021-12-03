64925e65284e00da9639f39fb58680ce3a340e34
Adrian Smijulj <adrian1358@gmail.com>
2021-12-03 13:01:09 +0100

    feat: create 5.19.0 upgrade script

--- 5.19.0/updateResolutions.js
+++ 5.19.0/updateResolutions.js
@@ -1,93 +1,20 @@
-const path = require("path");
-const fs = require("fs");
-const util = require("util");
-const ncpBase = require("ncp");
-const ncp = util.promisify(ncpBase.ncp);
-const { log } = require("../utils");
+const { log, addResolutionToRootPackageJson } = require("../utils");
 
-module.exports = async context => {
-    log.info(`Inserting new Admin Area and Website CLI plugins...`);
-    console.log();
-
-    const project = context.project;
-    const projectTsPath = path.join(project.root, "webiny.project.ts");
-
-    if (!fs.existsSync(projectTsPath)) {
-        log.warning(
-            `Skipping inserting new Admin Area and Website CLI plugins - ${log.warning.hl(
-                projectTsPath
-            )} does not exist.`
-        );
-        console.log();
-        return;
-    }
-
-    let projectTs = fs.readFileSync(projectTsPath, "utf8");
-
-    const apps = {
-        admin: fs.existsSync(path.join(project.root, "apps", "admin")),
-        website: fs.existsSync(path.join(project.root, "apps", "website"))
-    };
-
-    const src = path.join(__dirname, "newCliPlugins", "admin", "cli");
-    const dest = path.join(project.root, "apps", "admin", "cli");
-    log.info(`Inserting new CLI plugins into ${log.info.hl(dest)}.`);
-
-    if (apps.admin) {
-        await ncp(src, dest);
-        log.success("CLI plugins successfully inserted.");
-    } else {
-        log.warning(
-            `Skipping insertion of new CLI plugins into ${log.warning.hl(
-                dest
-            )}, it seems that the ${log.warning.hl(
-                "Admin Area"
-            )} project application doesn't exist.`
-        );
-    }
-    console.log();
-
-    const src2 = path.join(__dirname, "newCliPlugins", "website", "cli");
-    const dest2 = path.join(project.root, "apps", "website", "cli");
-    log.info(`Inserting new CLI plugins into ${log.info.hl(dest2)}.`);
-    if (apps.website) {
-        await ncp(src2, dest2);
-        log.success("CLI plugins successfully inserted.");
-    } else {
-        log.warning(
-            `Skipping insertion of new CLI plugins into ${log.warning.hl(
-                dest2
-            )}, it seems that the ${log.warning.hl("Website")} project application doesn't exist.`
-        );
-    }
-
-    console.log();
-
-    // Adjust root webiny.project.ts - update plugins.
-    log.info(`Updating root ${log.info.hl(`webiny.project.ts`)} manifest file...`);
-
-    projectTs = projectTs.replace('import cliPageBuilder from "@webiny/api-page-builder/cli";', "");
-
-    projectTs = projectTs.replace(
-        'import cliWorkspaces from "@webiny/cli-plugin-workspaces";',
-        `import cliWorkspaces from "@webiny/cli-plugin-workspaces";
-// Admin Area and Website CLI plugins.
-${apps.admin ? 'import adminPlugins from "./apps/admin/cli";' : ""}
-${apps.website ? 'import websitePlugins from "./apps/website/cli";' : ""}`
+module.exports = async () => {
+    log.info(
+        `Inserting new entries into the ${log.info.hl(
+            "resolutions"
+        )} property in the root ${log.info.hl("package.json")} file...`
     );
 
-    projectTs = projectTs.replace("cliPageBuilder(),", ``);
+    await addResolutionToRootPackageJson("package.json", {
+        "@pulumi/pulumi": "3.19.0",
+        "@pulumi/aws": "4.28.0"
+    });
 
-    projectTs = projectTs.replace(
-        "cliWorkspaces(),",
-        `cliWorkspaces(),
-            // Admin Area and Website CLI plugins.
-            ${apps.admin ? "adminPlugins," : ""}
-            ${apps.website ? "websitePlugins," : ""}`
-    );
-
-    fs.writeFileSync(projectTsPath, projectTs);
-    log.success(`Root ${log.success.hl(`webiny.project.ts`)} manifest file successfully updated.`);
+    log.success(`The following resolutions entries were successfully added:`);
+    console.log(`‣ "@pulumi/pulumi": "3.19.0",`)
+    console.log(`‣ "@pulumi/aws": "4.28.0"`)
 
     console.log();
 };
