ec3d8043f36b95c769cd007b450bf423edd2566f
Bruno Zorić <bruno.zoric@gmail.com>
2022-05-25 11:38:50 +0200

    feat: add upgrade for 5.25.0

---
+++ 5.25.0/index.js
@@ -0,0 +1,17 @@
+const glob = require("fast-glob");
+const { yarnInstall } = require("../utils");
+const reactTypes = require("./reactTypes");
+
+module.exports = async context => {
+    /**
+     * Copy the react types files from cwp-template-aws
+     */
+    await reactTypes.run(context);
+
+    /**
+     * Install new packages.
+     */
+    await yarnInstall({
+        context
+    });
+};
---
+++ 5.25.0/reactTypes.js
@@ -0,0 +1,31 @@
+const { log } = require("../utils");
+const path = require("path");
+const fs = require("fs-extra");
+const target = "node_modules/@webiny/cwp-template-aws/template/common/types/react";
+
+const copyTypesToUserProject = async context => {
+    if (!context.project || !context.project.root) {
+        log.error(
+            "Missing context.project or context.project.root to upgrade the Pulumi vpc.ts file."
+        );
+        return;
+    }
+
+    const source = path.join(context.project.root, target);
+    const destination = path.join(context.project.root, "types/react/");
+
+    if (fs.pathExistsSync(destination) === true) {
+        log.error(`Path "${destination}" already exists, skipping...`);
+        return;
+    }
+
+    log.info(`Copying from "${source}" to "${destination}"...`);
+    fs.copySync(source, destination, {
+        overwrite: false
+    });
+    log.info("... done");
+};
+
+module.exports = {
+    run: copyTypesToUserProject
+};
