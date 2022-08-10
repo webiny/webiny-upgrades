9455615eb8112b7e87b47a7f0746f8696ead347d
Bruno Zorić <bruno.zoric@gmail.com>
2022-08-10 10:36:50 +0200

    feat: upgrade to project to work with ts

--- 5.30.0/index.js
+++ 5.30.0/index.js
@@ -1,11 +1,3 @@
-/**
- * This upgrade uses ts-node, but it's local only to this upgrade.
- */
-process.env.NODE_PATH = process.cwd();
-require("ts-node").register({
-    dir: __dirname
-});
-
 const { upgradeProject } = require("./upgradeProject");
 
 module.exports = async context => {
--- types.ts
+++ types.ts
@@ -1,6 +1,18 @@
+import { PackageJson as BasePackageJson } from "type-fest";
+
 export interface Context {
     version: string;
     project: {
         root: string;
+    };
+}
+
+export interface Packages {
+    [key: string]: string;
+}
+
+export interface PackageJson extends BasePackageJson {
+    workspaces: {
+        packages: string[];
     };
 }
