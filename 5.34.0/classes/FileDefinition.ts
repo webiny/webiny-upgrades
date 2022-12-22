3e336a21e87f9c584aa65d840bb17891e13da25b
Leonardo Giacone <leonardo.giacone@gmail.com>
2022-12-22 14:19:44 +0100

    feat: upgrade pageBuilder to 5.34.0

---
+++ 5.34.0/classes/FileDefinition.ts
@@ -0,0 +1,24 @@
+export type Tag = "fm" | "pb" | "cms" | "gql" | "ps" | "ddb2es";
+export interface FileDefinitionParams {
+    path: string;
+    elasticsearch?: boolean;
+    pre529?: boolean;
+    tag: Tag;
+    name: string;
+}
+
+export class FileDefinition {
+    public readonly path: string;
+    public readonly elasticsearch: boolean;
+    public readonly pre529: boolean;
+    public readonly tag: Tag;
+    public readonly name: string;
+
+    public constructor(params: FileDefinitionParams) {
+        this.path = params.path;
+        this.elasticsearch = params.elasticsearch === true;
+        this.pre529 = params.pre529 === true;
+        this.tag = params.tag;
+        this.name = params.name;
+    }
+}
