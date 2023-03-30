f7dd4ed280cf29b86d5b84ed0458c306322a83e0
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-03-30 11:59:24 +0200

    feat: add api-page-builder-aco deps to api graphql

--- 5.35.0/updateGraphQL.ts
+++ 5.35.0/updateGraphQL.ts
@@ -1,13 +1,16 @@
 import { Project } from "ts-morph";
 import { Context } from "../types";
 import {
+    addPackagesToDependencies,
     addPluginToCreateHandler,
-    removePluginFromCreateHandler,
-    insertImportToSourceFile,
+    createFilePath,
     Files,
-    removeImportFromSourceFile,
+    findVersion,
+    getGraphQLPath,
     getIsElasticsearchProject,
-    getGraphQLPath
+    insertImportToSourceFile,
+    removeImportFromSourceFile,
+    removePluginFromCreateHandler
 } from "../utils";
 
 interface Params {
@@ -24,6 +27,8 @@ const updateIndexFile = async (params: Params): Promise<void> => {
     const { context, project, files } = params;
 
     const indexFile = files.byName("graphql/index");
+    const graphQLPackagePath = createFilePath(context, "${graphql}/package.json");
+    const version = findVersion(graphQLPackagePath) || context.version;
 
     if (!indexFile) {
         context.log.error(`Missing GraphQL index file (%s).`, "skipping upgrade");
@@ -90,5 +95,27 @@ const updateIndexFile = async (params: Params): Promise<void> => {
         source,
         value: "createFileManagerGraphQL()",
         before: "fileManagerS3()"
+    });
+
+    /**
+     * Add api-page-builder-aco dependencies to package.json
+     */
+    addPackagesToDependencies(context, graphQLPackagePath, {
+        "@webiny/api-page-builder-aco": version
+    });
+
+    /**
+     * Add api-page-builder-aco handlers to GraphQL
+     */
+    insertImportToSourceFile({
+        source,
+        name: ["createAcoPageBuilderContext"],
+        moduleSpecifier: "@webiny/api-page-builder-aco"
+    });
+
+    addPluginToCreateHandler({
+        source,
+        before: "scaffoldsPlugins",
+        value: "createAcoPageBuilderContext()"
     });
 };
