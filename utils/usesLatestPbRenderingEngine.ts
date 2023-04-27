6d52ed07c135b6de564fbf617f30d6577252386b
adrians5j <adrian@webiny.com>
2023-04-27 12:06:40 +0200

    fix: correct global.scss path

--- utils/usesLatestPbRenderingEngine.ts
+++ utils/usesLatestPbRenderingEngine.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 export const usesLatestPbRenderingEngine = (projectRoot = process.cwd()) => {
     const themeTsExists = fs.existsSync(`${projectRoot}/apps/theme/theme.ts`);
-    const globalScssExists = fs.existsSync(`${projectRoot}/apps/theme/global.ts`);
+    const globalScssExists = fs.existsSync(`${projectRoot}/apps/theme/global.scss`);
 
     return themeTsExists && globalScssExists;
 }
