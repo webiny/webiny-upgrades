b30a3ec821c836e4ee5873783386983ea8609929
Bruno Zorić <bruno.zoric@gmail.com>
2024-03-29 08:59:57 +0100

    fix: check if type was already updated

--- 5.40.0/updateForAwsSdk.ts
+++ 5.40.0/updateForAwsSdk.ts
@@ -1,10 +1,15 @@
 import { createProcessor } from "../utils";
 
 export const updateForAwsSdk = createProcessor(async params => {
-    const { project, files, context } = params;
+    const { project, files } = params;
 
     const file = files.byName("api/graphql/security");
     const source = project.getSourceFile(file.path);
 
-    source.replaceWithText(source.getText().replace("DynamoDBClient", "DynamoDBDocument"));
+    const text = source.getText();
+    if (!text || text.includes("DynamoDBDocument")) {
+        return;
+    }
+
+    source.replaceWithText(text.replace("DynamoDBClient", "DynamoDBDocument"));
 });
