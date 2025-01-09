b9e0615983456335093d2dd829e031bfdf786920
Bruno Zorić <bruno.zoric@gmail.com>
2025-01-09 12:20:10 +0100

    fix: yarn to 4.6.0

--- 5.42.0/updatesForYarn.ts
+++ 5.42.0/updatesForYarn.ts
@@ -2,7 +2,7 @@ import { createProcessor, updateYarn } from "../utils";
 
 export const updatesForYarn = createProcessor(async params => {
     const { context } = params;
-    const version = "4.5.3";
+    const version = "4.6.0";
 
     await updateYarn({
         context,
