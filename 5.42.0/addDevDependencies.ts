07f8f688f1a6eba04735017fb344defc53062084
Bruno Zorić <bruno.zoric@gmail.com>
2025-01-29 08:39:08 +0100

    fix: use exact versions instead of carets

--- 5.42.0/addDevDependencies.ts
+++ 5.42.0/addDevDependencies.ts
@@ -1,33 +1,28 @@
-import {
-    addPackagesToDevDependencies,
-    addPackagesToResolutions,
-    createProcessor,
-    yarnInstall
-} from "../utils";
+import { addPackagesToDevDependencies, createProcessor, yarnInstall } from "../utils";
 
 export const addDevDependencies = createProcessor(async ({ context, files }) => {
     const packages = {
-        "@babel/cli": "^7.26.4",
-        "@babel/code-frame": "^7.26.2",
-        "@babel/compat-data": "^7.26.3",
-        "@babel/core": "^7.26.0",
-        "@babel/helper-define-polyfill-provider": "^0.6.3",
-        "@babel/helper-environment-visitor": "^7.24.7",
-        "@babel/parser": "^7.26.3",
-        "@babel/plugin-proposal-class-properties": "^7.18.6",
-        "@babel/plugin-proposal-object-rest-spread": "^7.20.7",
-        "@babel/plugin-proposal-throw-expressions": "^7.25.9",
-        "@babel/plugin-syntax-object-rest-spread": "^7.8.3",
-        "@babel/plugin-transform-modules-commonjs": "^7.26.3",
-        "@babel/plugin-transform-runtime": "^7.25.9",
-        "@babel/preset-env": "^7.26.0",
-        "@babel/preset-react": "^7.26.3",
-        "@babel/preset-typescript": "^7.26.0",
-        "@babel/register": "^7.25.9",
-        "@babel/runtime": "^7.26.0",
-        "@babel/template": "^7.25.9",
-        "@babel/traverse": "^7.26.4",
-        "@babel/types": "^7.26.3"
+        "@babel/cli": "7.26.4",
+        "@babel/code-frame": "7.26.2",
+        "@babel/compat-data": "7.26.3",
+        "@babel/core": "7.26.0",
+        "@babel/helper-define-polyfill-provider": "0.6.3",
+        "@babel/helper-environment-visitor": "7.24.7",
+        "@babel/parser": "7.26.3",
+        "@babel/plugin-proposal-class-properties": "7.18.6",
+        "@babel/plugin-proposal-object-rest-spread": "7.20.7",
+        "@babel/plugin-proposal-throw-expressions": "7.25.9",
+        "@babel/plugin-syntax-object-rest-spread": "7.8.3",
+        "@babel/plugin-transform-modules-commonjs": "7.26.3",
+        "@babel/plugin-transform-runtime": "7.25.9",
+        "@babel/preset-env": "7.26.0",
+        "@babel/preset-react": "7.26.3",
+        "@babel/preset-typescript": "7.26.0",
+        "@babel/register": "7.25.9",
+        "@babel/runtime": "7.26.0",
+        "@babel/template": "7.25.9",
+        "@babel/traverse": "7.26.4",
+        "@babel/types": "7.26.3"
     };
 
     const targetPath = context.project.getPackageJsonPath();
@@ -37,14 +32,6 @@ export const addDevDependencies = createProcessor(async ({ context, files }) => 
      * We want to add packages to dev dependencies.
      */
     addPackagesToDevDependencies(context, targetPath, packages);
-    /**
-     * ... And to resolutions.
-     */
-    addPackagesToResolutions({
-        context,
-        packages,
-        targetPath
-    });
 
     await yarnInstall({
         quiet: true
